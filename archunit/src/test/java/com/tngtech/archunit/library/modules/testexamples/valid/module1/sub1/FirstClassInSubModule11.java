269384c920c56edff1766f825f4924cdb3b18b05
Peter Gafert <peter.gafert@tngtech.com>
2022-12-27 15:12:54 +0700

    add modules API to better support modularization rules
    This first step will allow to more easily modularize a code base by introducing the concept of an ArchUnit module. This can help in cases where other modularization techniques (e.g. Java Modules) are not available or feasible. The concept of a module borrows some common ideas from the Slices API, namely that every module will be uniquely identified by a module identifier and a module is principally a mapping `JavaClass` -> `ArchModule.Identifier`, which will partition the source space into buckets of classes having the same identifier.
    
    While principally the Slices API can already be used to write modularization rules, the mental model is a little off. From watching questions on Stackoverflow and ArchUnit issues I always got the feeling that users don't see the possibility to use the Slices API for these purposes, but more for just cutting the code base in slices and asserting freedom of cycles. While this was the original main use case, it now falls short to be noticed as solution for typical modularization problems.
    
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

---
+++ archunit/src/test/java/com/tngtech/archunit/library/modules/testexamples/valid/module1/sub1/FirstClassInSubModule11.java
@@ -0,0 +1,16 @@
+package com.tngtech.archunit.library.modules.testexamples.valid.module1.sub1;
+
+import java.util.List;
+
+import com.tngtech.archunit.library.modules.testexamples.valid.module1.sub2.FirstClassInSubModule12;
+import com.tngtech.archunit.library.modules.testexamples.valid.module1.sub2.SecondClassInSubModule12;
+
+@SuppressWarnings("unused")
+public class FirstClassInSubModule11 {
+    SecondClassInSubModule11 noDependencyToOtherModule;
+    FirstClassInSubModule12 firstDependencyOnSubModule12;
+    SecondClassInSubModule12 SecondDependencyOnSubModule12;
+
+    String firstUndefinedDependency;
+    List<?> secondUndefinedDependency;
+}
